0da2b31f8b7b1249bfb781278f1983c780b568d1
theNightCoders <suraj.sheikh@hotmail.com>
2024-05-29 18:22:19 +0530

    Python Fundamentals 1/4

---
+++ python_fundamentals.py
@@ -0,0 +1,44 @@
+# Declaration of variables
+num_type = 1
+string_type = "This is a string variable"
+float_type = 1.0
+boolean_type = True
+
+# Printing variables
+print(num_type)
+print(string_type)
+print(float_type)
+print(boolean_type)
+print(string_type + string_type)
+
+# Operators in Python
+print(5 + 5)  # Addition
+print(5 - 5)  # Subtraction
+print(5 * 5)  # Multiplication
+print(5 / 5)  # Division
+print(5 % 2)  # Modulus: This returns the remainder
+print(5 ** 5)  # Exponentiation 5^5
+print(5 // 5)  # Floor division: This returns the quotient without the remainder 5/2
+
+# Assignment operators
+a = 5
+a += 5  # a = a + 5
+a -= 5  # a = a - 5
+a *= 5  # a = a * 5
+a /= 5  # a = a / 5
+a %= 5  # a = a % 5
+a **= 5  # a = a ** 5
+a //= 5  # a = a // 5
+
+# Comparison operators
+print(5 == 5)  # Equal to
+print(5 != 5)  # Not equal to !=
+print(5 > 5)  # Greater than
+print(5 < 5)  # Less than
+print(5 >= 5)  # Greater than or equal to
+print(5 <= 5)  # Less than or equal to
+
+# Logical operators
+print(5 and 5)  # Logical AND
+print(5 or 5)  # Logical OR
+print(not 5)  # Logical NOT
